45884552cb885b43cf3ef2b6e7949915796c05cf
NouKD <NouKD-n09916538@gmail.com>
2020-03-01 16:52:04 +0000

    les exercices1et2

--- exo_soro.py
+++ exo_soro.py
@@ -1,3 +1,32 @@
+# Exercice 1
+#Écrire, avec des comparaisons, un algorithme qui affiche l’état de l’eau (glace, liquide, vapeur) en fonction de sa température.
+
+while True:
+    temperature = input("la température de l'eau: ")
+    if not temperature:
+        break
+    temperature = int(temperature)
+    if temperature <= 0:
+        print("\nC'est de la Glace")
+    elif 0< temperature < 100:
+        print("\nC'est du Liquide ") 
+    else:
+        print("\nC'est de la Vapeur")
+
+# Exercice 2
+#Écrire le même algorithme, mais en utilisant deux variables booléennes pour vérifier l’état de l’eau, sans comparaisons dans les SI.
+
+while True:
+    temperature = input("température de l'eau: ")
+    if not temperature:
+        break
+    temperature = int(temperature)
+    a = (temperature <= 0)
+    b = (0 < temperature < 100)
+    msg = "c'est de la Glace !" if a else "c'est du Liquide " if b else "Oups de la Vapeur"
+    print("\n", msg)
+    
+
 # exo 4
 def exo4():
     p = 0.9
